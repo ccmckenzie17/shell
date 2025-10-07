365097fed932ad81798a104b01d0f9fd0d964911
Cory McKenzie <cory.mckenzie@mail.utoronto.ca>
2025-10-07 19:13:08 -0400

    this version will work for sure

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -43,20 +43,21 @@ cd processed
 mkdir server_logs user_logs event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-find "/Users/corymckenzie/Desktop/shell/02_activities/assignments/newproject/data/raw" -name "*server*.log" -exec cp {} "/Users/corymckenzie/Desktop/shell/02_activities/assignments/newproject/data/processed/server_logs" \;
+cd ..
+cd ..
+cp ./data/raw/*server*.log ./data/processed/server_logs/
+
 
 # 6. Repeat the above step for user logs and event logs
-find "/Users/corymckenzie/Desktop/shell/02_activities/assignments/newproject/data/raw" -name "*user*.log" -exec cp {} "/Users/corymckenzie/Desktop/shell/02_activities/assignments/newproject/data/processed/user_logs" \;
-find "/Users/corymckenzie/Desktop/shell/02_activities/assignments/newproject/data/raw" -name "*event*.log" -exec cp {} "/Users/corymckenzie/Desktop/shell/02_activities/assignments/newproject/data/processed/event_logs" \;
+cp ./data/raw/*user*.log ./data/processed/user_logs/
+cp ./data/raw/*event*.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-cd "/Users/corymckenzie/Desktop/shell/02_activities/assignments/newproject/data/raw"
-rm *ipaddr*.*
-cd "/Users/corymckenzie/Desktop/shell/02_activities/assignments/newproject/data/processed/user_logs"
-rm *ipaddr*.*
+rm ./data/raw/*ipaddr*.*
+rm ./data/processed/user_logs/*ipaddr*.*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-cd "/Users/corymckenzie/Desktop/shell/02_activities/assignments/newproject/data"
+cd data
 ls -R > inventory.txt
 
 ###########################################
